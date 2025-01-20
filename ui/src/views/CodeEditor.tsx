76340b57bf87192185aa0a4be945470bd4e3c9de
Lorincz Mate <lorincz.mate@gmail.com>
2025-01-20 16:35:33 +0100

    Added react-simple-code-editor
    The issues:
    * The beforeunload API is very strange
    * Currently I use the JS highlighting
    * The save button is ugly
    * There is no Run button
    
    Idea: Two types of files:
    * lgl : logo library. Files with this extension are executed after save
      immadiatly. Can be used to define functions
    * lgs : logo script. Files with this extension must be executed
      manually. Can be used to define scripts.
    
    When a project is opened, the lgl-s should be executed.

--- ui/src/views/CodeEditor.tsx
+++ ui/src/views/CodeEditor.tsx
@@ -1,12 +1,23 @@
 import { Interpreter } from "web-logo-core";
 import { readFile } from '../utils/FileHandling'
 import { useEffect, useState } from "react";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from 'prismjs';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css'; //Example style, you can use another
+
 
 export default function CodeEditor({ interpreter }: { interpreter: Interpreter }) {
   const sharedData = (window as unknown as (Window & { sharedData: { fileHandle: FileSystemFileHandle } })).sharedData;
   const openedFile = sharedData.fileHandle;
-
+  const [saved, setSaved] = useState(true);
   const [fileContent, setFileContent] = useState<string | null>(null);
+
+  const save = () => {
+    setSaved(true);
+  }
+  
 
   useEffect(() => {
     let isMounted = true;
@@ -23,9 +34,45 @@ export default function CodeEditor({ interpreter }: { interpreter: Interpreter }
     };
   }, [openedFile]);
 
+  useEffect(() => {
+    let handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      // Prompt the user before closing the popup
+      let message = true;
+      event.returnValue = message; 
+      return message;
+    };
+    
+    if (!saved) {
+      window.addEventListener("beforeunload", handleBeforeUnload);
+    }
+
+    return () => {
+      if (!saved) {
+        window.removeEventListener("beforeunload", handleBeforeUnload);
+      }
+    };
+  }, [saved]);
+
   return (
     <div>
-      {fileContent === null ? <h1>Loading...</h1> : <h1>{fileContent}</h1>}
+      <button onClick={save}>Save</button>
+      {fileContent === null ? <h1>Loading...</h1> :
+        <Editor
+          value={fileContent}
+          onValueChange={code => {
+            console.log("On Value changed")
+            setSaved(false);
+            setFileContent(code);
+          }}
+          highlight={code => highlight(code, languages.js, "javascript")}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 12,
+          }}
+          
+        />
+      }
     </div>
   );
 }
