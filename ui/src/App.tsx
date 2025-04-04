332b6217b3be63664400fd107b72d934765d8de3
Lorincz Mate <lorincz.mate@gmail.com>
2025-04-04 18:13:24 +0200

    Fix popup basename for github pages

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,12 +3,13 @@ import { Interpreter } from 'web-logo-core';
 import Workspace from './views/Workspace';
 import { BrowserRouter, Route, Router, Routes, useNavigate } from 'react-router-dom';
 import CodeEditor from './views/CodeEditor';
+import config from './config';
 
 function App() {
   const interpreter = useRef<Interpreter>(new Interpreter());
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={config.basename}>
       <Routes>
         <Route path="/" element={<Workspace interpreter={interpreter.current}/>} />
         <Route path="/code-editor" element={<CodeEditor/>} />
