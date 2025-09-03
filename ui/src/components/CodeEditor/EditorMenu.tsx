fa1b9550a09f29e28c77daa040df51fe44dba526
Lorincz Mate <lorincz.mate@gmail.com>
2025-09-04 01:30:26 +0200

    Sections can be executed independently from code editor

--- ui/src/components/CodeEditor/EditorMenu.tsx
+++ ui/src/components/CodeEditor/EditorMenu.tsx
@@ -2,12 +2,14 @@ import "./menu.css"
 
 type EditorMenuProps = {
   run: () => Promise<void>,
+  runSection: () => Promise<void>,
   save: () => Promise<void>,
 }
 
-export default function EditorMenu({ run, save }: EditorMenuProps) {
+export default function EditorMenu({ run, runSection, save }: EditorMenuProps) {
   return <div className="menu-header">
     <button className={"menu-button"} onClick={run}>Run</button>
+    <button className={"menu-button"} onClick={runSection}>Run Section</button>
     <button className={"menu-button"} onClick={save}>Save</button>
   </div>
 }
