95ea8b09a9e8c40ecbbbe54eac655baaf10d8b98
Lorincz Mate <lorincz.mate@gmail.com>
2025-09-03 22:58:39 +0200

    Add comment syntax highlight

--- ui/src/components/CodeEditor/LogoSyntax.ts
+++ ui/src/components/CodeEditor/LogoSyntax.ts
@@ -3,22 +3,26 @@ import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/themes/prism.css';
 
-export default function getLogoLanguagePrismModel(keywords : string[]) {
-    return languages.extend('clike', {
-      keyword: new RegExp(`\\b(?:${keywords.join('|')})\\b`),
-      number: /\b(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
-      operator: {
-          pattern: /(^|[^.])(?:\+\+|--|&&|\|\||->|=>|<<|>>>?|==|!=|[<>]=?|[+\-*/%&|^=!<>])(?!=)/,
-          lookbehind: true
-      },
-      string: {
-        pattern: /"(?:\\.|[^"\\])*"/, // Only double-quoted strings
-        greedy: true
-      },    
-      charlikeNumber: {
-        pattern: /'(?:\\.|[^\s\\])/, // Matches `'a` or `'\n`
-        alias: 'number'
-      }
-    });
-  }
-  
+export default function getLogoLanguagePrismModel(keywords: string[]) {
+  return languages.extend('clike', {
+    keyword: new RegExp(`\\b(?:${keywords.join('|')})\\b`),
+    number: /\b(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i,
+    operator: {
+      pattern: /(^|[^.])(?:\+\+|--|&&|\|\||->|=>|<<|>>>?|==|!=|[<>]=?|[+\-*/%&|^=!<>])(?!=)/,
+      lookbehind: true
+    },
+    string: {
+      pattern: /"(?:\\.|[^"\\])*"/, // Only double-quoted strings
+      greedy: true
+    },
+    charlikeNumber: {
+      pattern: /'(?:\\.|[^\s\\])/, // Matches `'a` or `'\n`
+      alias: 'number'
+    },
+    comment: {
+      pattern: /([^'])#.*/,
+      greedy: true,
+      lookbehind: true,
+    },
+  });
+}
